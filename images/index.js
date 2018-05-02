4885da3ebcc345331c2ce012986a2fe74410e40f
Olagunju daniel <todak2000@gmail.com>
2018-05-02 11:54:17 +0100

    esfds

--- images/index.js
+++ images/index.js
@@ -1,6 +1,5 @@
 
-funcion addName (){
-    var nameInput = document.getElementById('#name').value;
+var nameInput = document.getElementById('#name').value;
 var hname = document.getElementById('h-name');
 hname.innerHTML = "Welcome" +nameInput;
-}
+
